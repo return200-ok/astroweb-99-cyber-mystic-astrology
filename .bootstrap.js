659adeb7f3acd19e4333331b70bd85bf1ba18824
Vibesdk <vibesdk-bot@cloudflare.com>
2026-03-17 04:08:25 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add lunar-javascript html-to-image"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
